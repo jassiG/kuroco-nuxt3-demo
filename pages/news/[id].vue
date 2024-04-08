ea4ea17889841f9161d595f61e6a1bded7276c5f
Jaswant Singh <jaswant_s@ch.iitr.ac.in>
2024-04-08 20:42:55 +0900

    Update [id].vue

--- pages/news/[id].vue
+++ pages/news/[id].vue
@@ -1,10 +1,12 @@
 <template>
-    <div>
-      <h1 class="title text-lg font-semibold text-gray-800" >{{ response.details.subject }}</h1>
-      <div class="post" v-html="response.details.contents"></div>
-    </div>
+  <div>
+    <h1 class="title text-lg font-semibold text-gray-800">
+      {{ response.details.subject }}
+    </h1>
+    <div class="post" v-html="response.details.contents"></div>
+  </div>
 </template>
-  
+
 <script setup>
 const route = useRoute();
 const config = useRuntimeConfig();
@@ -12,8 +14,7 @@ const config = useRuntimeConfig();
 const { data: response } = await useFetch(
   `${config.public.apiBase}/rcms-api/1/newsdetail/${route.params.id}`,
   {
-    credentials: 'include',
+    credentials: "include",
   }
 );
-
 </script>
