5538fbb88e8e23bb309ade546a906369bfb54558
Jaswant Singh <jaswant_s@ch.iitr.ac.in>
2024-04-08 20:42:28 +0900

    bugfix

--- pages/form/form_validate.vue
+++ pages/form/form_validate.vue
@@ -1,87 +1,123 @@
 <template>
-    <div>
-      <section>
-        <h1 class="text-xl text-gray-800">Contact</h1>
-        <form v-if="!validated" class="input">
-          <div v-if="error" class="text-red-500">
-            <p v-for="(err, idx) in error" :key="idx">
-              {{ err }}
-            </p>
-          </div>
+  <div>
+    <section>
+      <h1 class="text-xl text-gray-800">Contact</h1>
+      <form v-if="!validated" class="input">
+        <div v-if="error" class="text-red-500">
+          <p v-for="(err, idx) in error" :key="idx">
+            {{ err }}
+          </p>
+        </div>
+        <div>
+          <dl>
+            <dt>Name</dt>
+            <dd>
+              <input
+                v-model="submitData.name"
+                name="name"
+                type="text"
+                :disabled="validated"
+                class="border border-slate-300 rounded"
+              />
+            </dd>
+          </dl>
+          <dl>
+            <dt>Email</dt>
+            <dd>
+              <input
+                v-model="submitData.email"
+                name="email"
+                type="text"
+                :disabled="validated"
+                class="border border-slate-300 rounded"
+              />
+            </dd>
+          </dl>
+          <dl>
+            <dt>Message</dt>
+            <dd>
+              <textarea
+                v-model="submitData.body"
+                name="body"
+                :disabled="validated"
+                class="border border-slate-300 rounded"
+              ></textarea>
+            </dd>
+          </dl>
+        </div>
+        <button
+          @click.prevent="handleOnValidate"
+          class="bg-slate-500 hover:bg-slate-700 text-white font-semibold py-2 px-4 rounded"
+        >
+          Confirm your entry
+        </button>
+      </form>
+      <form v-if="validated" class="confirm">
+        <div v-if="submitted">Inquiry submitted.</div>
+        <div v-else>
           <div>
-            <dl>
-              <dt>Name</dt>
-              <dd><input v-model="submitData.name" name="name" type="text" :disabled="validated" class="border border-slate-300 rounded"></dd>
-            </dl>
-            <dl>
-              <dt>Email</dt>
-              <dd><input v-model="submitData.email" name="email" type="text" :disabled="validated" class="border border-slate-300 rounded"></dd>
-            </dl>
-            <dl>
-              <dt>Message</dt>
-              <dd><textarea v-model="submitData.body" name="body" :disabled="validated" class="border border-slate-300 rounded"></textarea></dd>
-            </dl>
-          </div>
-          <button @click.prevent="handleOnValidate" class="bg-slate-500 hover:bg-slate-700 text-white font-semibold py-2 px-4 rounded">Confirm your entry</button>
-        </form>
-        <form v-if="validated" class="confirm">
-          <div v-if="submitted">Inquiry submitted.</div>
-          <div v-else>
-            <div>
-              <div v-for="field in ['Name', 'Email', 'Body']" class="">
-                <div>{{ field }}</div>
-                <div class="text-slate-600">{{submitData[field.toLowerCase()]}}</div>
+            <div v-for="field in ['Name', 'Email', 'Body']" class="">
+              <div>{{ field }}</div>
+              <div class="text-slate-600">
+                {{ submitData[field.toLowerCase()] }}
               </div>
             </div>
-            <div class="my-2 space-x-4">
-              <button @click.prevent="handleOnSubmit" class="bg-slate-500 hover:bg-slate-700 text-white font-semibold py-2 px-4 rounded">Submit</button>
-              <button @click.prevent="validated = false" class="bg-slate-500 hover:bg-slate-700 text-white font-semibold py-2 px-4 rounded">Back</button>
-            </div>
           </div>
-        </form>
-      </section>
-    </div>
-  </template>
-  
-  <script setup>
-  const submitted = ref(false);
-  const validated = ref(false);
-  const submitData = ref({});
-  const error = ref(null);
-  const config = useRuntimeConfig();
-  
-  const handleOnValidate = async () => {
-    //Validate the entry
-    try {
-      await $fetch('/rcms-api/1/form_validate', {
-        method: 'POST',
-        body: { ...submitData.value },
-        baseURL: config.public.apiBase,
-        credentials: 'include',
-      });
-      validated.value = true;
-      error.value = null;
-    } catch (e) {
-      error.value = e;
-      console.log('error', e);
-    }
-  };
-  
-  const handleOnSubmit = async () => {
-    //Post processing to Kuroco endpoints
-    try {
-      await $fetch('/rcms-api/1/form_send', {
-        method: 'POST',
-        body: { ...submitData.value },
-        baseURL: config.public.apiBase,
-        credentials: 'include',
-      }).then((res) => {
-        console.log('res', res);
-      })
-      submitted.value = true;
-    } catch (e) {
-      error.value = e;
-      console.log('error', e);
-    }
-  };
-  </script>
+          <div class="my-2 space-x-4">
+            <button
+              @click.prevent="handleOnSubmit"
+              class="bg-slate-500 hover:bg-slate-700 text-white font-semibold py-2 px-4 rounded"
+            >
+              Submit
+            </button>
+            <button
+              @click.prevent="validated = false"
+              class="bg-slate-500 hover:bg-slate-700 text-white font-semibold py-2 px-4 rounded"
+            >
+              Back
+            </button>
+          </div>
+        </div>
+      </form>
+    </section>
+  </div>
+</template>
+
+<script setup>
+const submitted = ref(false);
+const validated = ref(false);
+const submitData = ref({});
+const error = ref(null);
+const config = useRuntimeConfig();
+
+const handleOnValidate = async () => {
+  //Validate the entry
+  try {
+    await $fetch("/rcms-api/1/form_validate", {
+      method: "POST",
+      body: { ...submitData.value },
+      baseURL: config.public.apiBase,
+      credentials: "include",
+    });
+    validated.value = true;
+    error.value = null;
+  } catch (e) {
+    error.value = e;
+  }
+};
+
+const handleOnSubmit = async () => {
+  //Post processing to Kuroco endpoints
+  try {
+    await $fetch("/rcms-api/1/form_send", {
+      method: "POST",
+      body: { ...submitData.value },
+      baseURL: config.public.apiBase,
+      credentials: "include",
+    });
+    submitted.value = true;
+  } catch (e) {
+    error.value = e;
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>
-        <h1 class="text-xl text-gray-800">Index</h1>
-        <ul class="my-2 space-y-4">
-            <!-- link buttons -->
-            <li><nuxt-link to="/news/" class="bg-slate-500 hover:bg-slate-700 text-white font-semibold py-2 px-4 rounded">News</nuxt-link></li>
-            <li><nuxt-link to="/form/form_validate"  class="bg-slate-500 hover:bg-slate-700 text-white font-semibold py-2 px-4 rounded">Form Validate</nuxt-link></li>
-        </ul>
-    </div>
+  <div>
+    <h1 class="text-xl text-gray-800">Index</h1>
+    <ul class="my-2 space-y-4">
+      <!-- link buttons -->
+      <li>
+        <nuxt-link
+          to="/news/"
+          class="bg-slate-500 hover:bg-slate-700 text-white font-semibold py-2 px-4 rounded"
+          >News</nuxt-link
+        >
+      </li>
+      <li>
+        <nuxt-link
+          to="/form/form_validate"
+          class="bg-slate-500 hover:bg-slate-700 text-white font-semibold py-2 px-4 rounded"
+          >Form Validate</nuxt-link
+        >
+      </li>
+    </ul>
+  </div>
 </template>
-  
-  
