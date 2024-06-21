16055ca15a8ce1ac64e610ea87162e294dba87a7
Jaswant Singh <jaswant_s@ch.iitr.ac.in>
2024-06-21 20:49:30 +0900

    Update form_validate.vue

--- pages/form/form_validate.vue
+++ pages/form/form_validate.vue
@@ -2,8 +2,8 @@
   <div>
     <section>
       <h1>Contact</h1>
-      <form v-if="!validated">
-        <div v-if="error" class="text-red-500">
+      <form v-if="!validated" class="input">
+        <div v-if="error" class="error">
           <p v-for="(err, idx) in error" :key="idx">
             {{ err }}
           </p>
@@ -11,49 +11,26 @@
         <div>
           <dl>
             <dt>Name</dt>
-            <dd>
-              <input
-                v-model="submitData.name"
-                name="name"
-                type="text"
-                :disabled="validated"
-              />
-            </dd>
+            <dd><input v-model="submitData.name" name="name" type="text" :disabled="validated"></dd>
           </dl>
           <dl>
             <dt>Email</dt>
-            <dd>
-              <input
-                v-model="submitData.email"
-                name="email"
-                type="text"
-                :disabled="validated"
-              />
-            </dd>
+            <dd><input v-model="submitData.email" name="email" type="text" :disabled="validated"></dd>
           </dl>
           <dl>
             <dt>Message</dt>
-            <dd>
-              <textarea
-                v-model="submitData.body"
-                name="body"
-                :disabled="validated"
-              ></textarea>
-            </dd>
+            <dd><textarea v-model="submitData.body" name="body" :disabled="validated"></textarea></dd>
           </dl>
         </div>
         <button @click.prevent="handleOnValidate">Confirm your entry</button>
       </form>
-      <form v-if="validated">
+      <form v-if="validated" class="confirm">
         <div v-if="submitted">Inquiry submitted.</div>
         <div v-else>
           <div>
-            <div v-for="field in ['Name', 'Email', 'Body']">
-              <div>{{ field }}</div>
-              <div>
-                {{ submitData[field.toLowerCase()] }}
-              </div>
-            </div>
+            <div>Name   :{{submitData.name}}</div>
+            <div>Email  :{{submitData.email}}</div>
+            <div>Message:{{submitData.body}}</div>
           </div>
           <div>
             <button @click.prevent="handleOnSubmit">Submit</button>
@@ -69,38 +46,37 @@
 const submitted = ref(false);
 const validated = ref(false);
 const submitData = ref({});
-const error = ref(null);
+const error = ref([]);
 const config = useRuntimeConfig();
 
 const handleOnValidate = async () => {
   //Validate the entry
-  const res = await useFetch("/rcms-api/1/form_validate", {
-    method: "POST",
-    body: { ...submitData.value },
-    baseURL: config.public.apiBase,
-    credentials: "include",
-  });
-  if (res.status.value == "error") {
-    error.value = res.error.value.data.errors.map(item => item['message']);
-  } else {
+  try {
+    await $fetch('/rcms-api/1/form_validate', {
+      method: 'POST',
+      body: { ...submitData.value },
+      baseURL: config.public.apiBase,
+      credentials: 'include',
+    });
     validated.value = true;
     error.value = null;
+  } catch (e) {
+    error.value = e.response._data.errors; // or error.value = e.data.errors
   }
 };
 
 const handleOnSubmit = async () => {
   //Post processing to Kuroco endpoints
-  const res = await useFetch("/rcms-api/1/form_send", {
-    method: "POST",
-    body: { ...submitData.value },
-    baseURL: config.public.apiBase,
-    credentials: "include",
-  });
-  if (res.status.value == "error") {
-    error.value = res.error.value.data.errors.map(item => item['message']);
-  } else {
+  try {
+    await $fetch('/rcms-api/1/form_send', {
+      method: 'POST',
+      body: { ...submitData.value },
+      baseURL: config.public.apiBase,
+      credentials: 'include',
+    });
     submitted.value = true;
-    error.value = null;
+  } catch (e) {
+    console.log(e);
   }
 };
 </script>
