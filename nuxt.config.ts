41e9fdb683ff2a0bd825652ac3fd9d9dd7834c32
Jaswant Singh <jaswant_s@ch.iitr.ac.in>
2024-08-13 23:02:14 +0900

    add i18 nuxt3 module and configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,25 @@ export default defineNuxtConfig({
       enabled: true,
     },
   },
-  modules: [],
+  modules: [
+    [
+      "@nuxtjs/i18n",
+      {
+        strategy: "prefix_and_default",
+        // Define the language options
+        locales: [
+          { code: "ja", file: "ja.json" },
+          { code: "en", file: "en.json" },
+        ],
+        // Set the default language
+        defaultLocale: "ja",
+        vueI18nLoader: true,
+        lazy: true,
+        // Specify the directory for the JSON file
+        langDir: "locales/",
+      },
+    ],
+  ],
   runtimeConfig: {
     // Public keys that are exposed to the client
     public: {
