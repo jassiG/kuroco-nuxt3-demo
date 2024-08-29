a376222b1f76962dd22afe289dfb370a4223554e
Jaswant Singh <jaswant_s@ch.iitr.ac.in>
2024-08-29 19:37:30 +0900

    revert default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
-    <header>
-      <nuxt-link :to="switchLocalePath('en')">{{ $t("links.en") }}</nuxt-link>
-      |
-      <nuxt-link :to="switchLocalePath('ja')">{{ $t("links.ja") }}</nuxt-link>
-    </header>
     <NuxtPage />
   </div>
 </template>
